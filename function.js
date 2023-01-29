c68dc24a17754b0f9c29827b2816c360609add16
FAHIMXGG <ahasanulhaquefahim1@gmail.com>
2023-01-29 23:12:54 +0600

    2nd

--- function.js
+++ function.js
@@ -1,1 +1,13 @@
-hello
+//function declaration
+function startFan(){
+    console.log('stand up');
+    console.log('walk towards the switch');
+    console.log('Oress the switch');
+}
+// call the function
+startFan();
+console.log('hello dude');
+startFan();
+console.log('eating lunch');
+console.log('watching Js tuto');
+startFan();
