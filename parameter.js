76aa9baf5b684f82df16ec4d680bb9431c842ecf
FAHIMXGG <ahasanulhaquefahim1@gmail.com>
2023-01-29 23:59:38 +0600

    mid

--- parameter.js
+++ parameter.js
@@ -5,3 +5,22 @@
 
 // bringSingara(100);
 
+// function giveSingara(money){
+//     console.log('eto taka disen:', money, '?');
+//     console.log('ey nan shingara');
+// }
+// giveSingara(300);
+
+//
+// function add(num1, num2) {
+//     console.log('Going to add', num1, num2, 'eygula e');
+// }
+
+// add(300, 43);
+//
+function sum(a, b, c, d, e, f, g, h) {
+    console.log(a,b,c,d,e,f,h,);
+    var sum = a+b+c+d+e+f+g+h;
+    console.log(sum);
+}
+sum(32,53,23,4,2,5,6,44,3);
---
+++ return.js
@@ -0,0 +1,14 @@
+// function add(num1, num2) {
+//     console.log(mum1, num2);
+//     var sum = num1+num2;
+//     return  sum;
+// }
+
+function add(num1, num2){
+    console.log(num1, num2);
+    var sum = num1 + num2;
+    return sum;
+}
+
+var total = add(44, 65);
+console.log("total", total);
